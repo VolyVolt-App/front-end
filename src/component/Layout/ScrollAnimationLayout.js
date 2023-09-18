b33f9a8b2f06c6b9f6870cb7afcfed542574674e
Roussel Rakoto <hei.roussel11@gmail.com>
2023-09-18 09:23:44 +0300

    feat: create scroll animation layout - component

---
+++ src/component/Layout/ScrollAnimationLayout.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const ScrollAnimationLayout = ({children}) => {
+  return (
+    <Box>
+        {children}
+    </Box>
+  )
+}
+
+export default ScrollAnimationLayout

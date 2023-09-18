ecc0fe06077044643b1533fb24bedc11de8ad079
Roussel Rakoto <hei.roussel11@gmail.com>
2023-09-18 09:52:00 +0300

    feat(draft): control scroll animation

--- src/component/Layout/ScrollAnimationLayout.js
+++ src/component/Layout/ScrollAnimationLayout.js
@@ -1,12 +1,31 @@
 import { Box } from '@mui/material'
-import React from 'react'
+import React, { useRef } from 'react'
+import { motion, useAnimation,useInView } from 'framer-motion';
+import { ScrollAnimationVariants } from '../Utils/AnimationVariants/HomeAnimationVariants';
 
 const ScrollAnimationLayout = ({children}) => {
-  return (
-    <Box>
-        {children}
-    </Box>
-  )
+    const ref = useRef(null);
+    const scrollAnimation = useAnimation();
+    const inView = useInView(ref, { once: true });
+
+    useEffect(() => {
+      if(inView) {
+        scrollAnimation.start('visible');
+      }
+    }, [inView])
+    
+
+    return (
+        <Box 
+            ref={ref} 
+            component={motion.div} 
+            variants={ScrollAnimationVariants} 
+            initial="hidden"
+            animate={scrollAnimation}
+        >
+            {children}
+        </Box>
+    )
 }
 
 export default ScrollAnimationLayout

7d461876ccc967d8adf9fe9d7662594aa55c051e
Fenomanana <fenomanana.nomenjanahary@gmail.com>
2023-09-21 21:45:45 +0300

    feat: my animation solution

--- src/component/solution/Analyse.jsx
+++ src/component/solution/Analyse.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import {  Typography, Box } from '@mui/material'
+import { AnimatePresence, motion } from 'framer-motion'
+import { processusVariants } from '../Utils/AnimationVariants/SolutionAnimationVariants'
 
 
 export const Analyse = ({text}) => {
   return (
-    <Box py={2} >
+    <AnimatePresence>
+    <Box component={motion.div}
+     py={2}
+     variants={processusVariants}
+     initial='hidden'
+     animate={'visible'}
+     exit={{
+      y:-10000,
+      //opacity: 0,
+  }}>
      <Typography align='center' sx={{color: '#5E840E'}}>
       {text}
      </Typography>
      
     </Box>
+    </AnimatePresence>
   )
 }

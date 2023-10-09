cda000c6959057a83bdf47c5c3681bb33a8af75e
Fenomanana <fenomanana.nomenjanahary@gmail.com>
2023-10-09 11:10:53 +0300

    3d and avantage client

--- src/services/BaseUrl.js
+++ src/services/BaseUrl.js
@@ -1,6 +1,6 @@
-export const ENDPOINT = 'https://127.0.0.1:8000/'
+//export const ENDPOINT = 'https://127.0.0.1:8000/'
 //export const ENDPOINT = 'https://192.168.137.1:8000/'
-//export const ENDPOINT = 'http://volyvolt.us-east-2.elasticbeanstalk.com/'
+export const ENDPOINT = 'http://volyvolt.us-east-2.elasticbeanstalk.com/'
 
-export const FRONTENDPOINT = 'http://localhost:3000/'
-//export const FRONTENDPOINT = 'https://volyvolt.org/'
+//export const FRONTENDPOINT = 'http://localhost:3000/'
+export const FRONTENDPOINT = 'https://volyvolt.org/'
